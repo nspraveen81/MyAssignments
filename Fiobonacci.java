31673f35f694d9adb493b1074930eccecf9425db
Praveen Narayanan <66739151+nspraveen81@users.noreply.github.com>
2024-04-29 08:53:29 +0100

    Add files via upload

---
+++ Fiobonacci.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class Fiobonacci {
+	
+	public static void main(String[] args) {
+		
+		int a=0;
+		int b = 1;
+		int sum = 8;
+		
+		for (int i = 1; i <=sum; i++) {
+			System.out.println(a);
+			int c = a +b;
+			a = b;
+			b = c;
+			
+			
+				
+				
+			}
+			
+			
+		}
+	}
+
+
+
